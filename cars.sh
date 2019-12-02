eaae240464d823840a7c28a29cee9625453d9da1
Debbie Lu <dlu@chapman.edu>
2019-12-02 01:32:53 +0000

    Fixed shellcheck errors for cars.sh

--- cars.sh
+++ cars.sh
@@ -9,15 +9,15 @@ do
 	echo "Type 1 to add a new car"
 	echo "Type 2 to display the list of cars"
 	echo "Type 3 to exit the program"
-	echo -n "Enter a number: "; read OPTION
+	echo -n "Enter a number: "; read -r OPTION
 	case "$OPTION" in
 		1)
 			echo "Enter the year: "
-			read YEAR
+			read -r YEAR
 			echo "Enter the make: "
-			read MAKE
+			read -r MAKE
 			echo "Enter the model: "
-			read MODEL
+			read -r MODEL
 			echo "${YEAR}:${MAKE}:${MODEL}" >> My_old_cars
 			;;
 		2)
